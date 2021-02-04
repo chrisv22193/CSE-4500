bc09634f5ba2d7292c2138edaadc10cdb02f899d
Chris Vazquez <chrisv22193@yahoo.com>
2021-02-03 20:48:55 -0800

    Delete api_test.js

--- api_test.js
+++
@@ -1,9 +0,0 @@
-const url = 'https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today'
-
-async function getAPI() {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-}
-
-getAPI();
